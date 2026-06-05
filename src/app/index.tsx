2a222f6ee6c94df337de1e940bd82829bb69c98c
Shreya pal <shreyapal7985@gmail.com>
2026-06-05 18:56:03 +0530

    fetching the data of api

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
+  const getdata= async()=>{
+    try{
+    const url="https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20"
+    const data= await fetch(url);
+    const result=await data.json();
+    console.log(result)
+  }
+catch(error){
+  console.log(error)
+} }
+useEffect(()=>{
+  getdata()
+},[])
+  
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text>Pokodex</Text>
     </View>
   );
 }
