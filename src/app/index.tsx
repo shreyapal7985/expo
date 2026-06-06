98e8a8dd52939c2743970f0fe9e5ed67b3519e7d
Shreya pal <shreyapal7985@gmail.com>
2026-06-07 01:18:03 +0530

    understand about results object

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { useEffect,useState } from "react";
 import { Text, View, StyleSheet, ScrollView } from "react-native";
 
 export default function Index() {
-  const [pokemon,setPokemon]=useState([])//we pass empty array for bulk of data
+  const [pokemon,setPokemon]=useState<any[]>([])//we pass empty array for bulk of data    any[] is the type of array value
 
   //Api fuction
   const getdata= async()=>{
@@ -10,7 +10,13 @@ export default function Index() {
     const url="https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20"
     const data= await fetch(url);
     const result=await data.json();
-    setPokemon(result);
+    setPokemon(result.results);
+  /*here results is a property inside the objects which contains the name of pokemons only result stores the whole api data ex-{
+  count: 1350,
+  next: "...",
+  previous: null,
+  results: [ ...pokemon list... ]
+}*/
   }
 catch(error){
   console.log(error)
@@ -23,7 +29,11 @@ useEffect(()=>{
   
   return (
     <ScrollView>
-      
+      {pokemon.map((pokemons) => (
+  <View key={pokemons.name}>
+    <Text>{pokemons.name}</Text>
+  </View>
+))}
     </ScrollView>
   );
 }
