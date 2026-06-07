9ef1d2d329954913046eb1a547877282c158173a
Shreya pal <shreyapal7985@gmail.com>
2026-06-07 17:25:01 +0530

    create detailedPokemons() displayed image, and add few comments

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,21 @@
 import { useEffect,useState } from "react";
-import { Text, View, StyleSheet, ScrollView } from "react-native";
+import { Text, View, StyleSheet, ScrollView, Image } from "react-native";
+
+interface Pokemon{
+  name:string
+  image:string
+  imageBack:string
+  types:PokemonType
+}
+
+interface PokemonType{
+  type:{
+  name:string,
+  url:string}
+}
 
 export default function Index() {
-  const [pokemon,setPokemon]=useState<any[]>([])//we pass empty array for bulk of data    any[] is the type of array value
+  const [pokemon,setPokemon]=useState<Pokemon[]>([])//we pass empty array for bulk of data    any[] is the type of array value
 
   //Api fuction
   const getdata= async()=>{
@@ -10,7 +23,21 @@ export default function Index() {
     const url="https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20"
     const data= await fetch(url);
     const result=await data.json();
-    setPokemon(result.results);
+
+// this function to fetch image of pokemon because it stores the image inside result:[{name:"bulbasaur",url:"https://pokeapi.co/api/v2/pokemon/21"}] inside this url our imgage is available
+    const detailedPokemons= await Promise.all(//group all promises into one promise and wait untill it's completion
+      result.results.map(async(pokemon:any)=>{//it maps all result data inside the local variable pokemon
+        const res = await fetch(pokemon.url);//this is the url inside the result object 
+        const details = await res.json();
+        return{
+          name:pokemon.name,//pokemon is local variable 
+          image:details.sprites.front_default,
+          imageBack:details.sprites.back_default,
+          types:details.types
+        };
+      })
+    );
+    setPokemon(detailedPokemons);
   /*here results is a property inside the objects which contains the name of pokemons only result stores the whole api data ex-{
   count: 1350,
   next: "...",
@@ -32,16 +59,18 @@ useEffect(()=>{
       {pokemon.map((pokemons) => (
   <View key={pokemons.name}>
     <Text>{pokemons.name}</Text>
+    <View style={{flexDirection:'row'}}>
+    <Image
+    source={{uri: pokemons.image}}
+    style={{height:100, width:100}}/>
+    <Image
+    source={{uri: pokemons.imageBack}}
+    style={{height:100, width:100}}/>
+  </View>
   </View>
 ))}
     </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
